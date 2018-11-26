61dd5ae9a2c06b3c7abe3d4d2a86595aa120a0f2
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-11-26 15:01:52 +0100

    add CollisionResult

---
+++ src/main/java/model/CollisionResult.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import model.gameobjects.GameObject;
+
+public class CollisionResult {
+	private List<GameObject> toRemove = new LinkedList<>();
+
+	public List<GameObject> getToRemove() {
+		return toRemove;
+	}
+
+	GameObject result;
+
+	public GameObject getResult() {
+		return result;
+	}
+
+	public void setResult(GameObject result) {
+		this.result = result;
+	}
+
+	public void addToToRemove(GameObject gameObject) {
+		this.toRemove.add(gameObject);
+	}
+
+}
