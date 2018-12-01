2a66b37955c18767c75404aa199437b641194409
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-12-01 23:21:08 +0100

    add docs to mapinitializer

--- src/main/java/model/MapInitializer.java
+++ src/main/java/model/MapInitializer.java
@@ -24,70 +24,108 @@ public class MapInitializer {
 				map.put(new Coordinates(i, j), new Field(new Coordinates(i, j)));
 			}
 		}
-		placeDoctor();
-		placePowerUp(generateNumberOfPowerUps(levelNumber), map);
-		placeDaleks(generateNumberOfDaleks(levelNumber), map);
-		placeStones(generateNumberOfStones(levelNumber), map);
-		placeTrees(generateNumberOfTrees(levelNumber), map);
+		placeDoctor(sizeOfMap);
+		placePowerUp(generateNumberOfPowerUps(levelNumber), map, sizeOfMap);
+		placeDaleks(generateNumberOfDaleks(levelNumber), map, sizeOfMap);
+		placeStones(generateNumberOfStones(levelNumber), map, sizeOfMap);
+		placeTrees(generateNumberOfTrees(levelNumber), map, sizeOfMap);
 		return new LevelMap(sizeOfMap, map);
 	}
 
-	private void placeDoctor() {
-		Game.doctor.setCoordinates(generateCoordinatesForDoctor());
+	private void placeDoctor(int sizeOfMap) {
+		Game.doctor.setCoordinates(generateCoordinatesForDoctor(sizeOfMap));
 	}
 
-	private void placeDaleks(int numberOfDaleks, Map<Coordinates, Field> map) {
+	private void placeDaleks(int numberOfDaleks, Map<Coordinates, Field> map, int sizeOfMap) {
 		for (int i = 0; i < numberOfDaleks; i++) {
-			Coordinates coordinates = generateCoordinatesForOther(map);
+			Coordinates coordinates = generateCoordinatesForOther(map, sizeOfMap);
 			map.get(coordinates).addDalek(new Dalek(coordinates));
 		}
 	}
 
-	private void placeTrees(int numberOfTrees, Map<Coordinates, Field> map) {
+	private void placeTrees(int numberOfTrees, Map<Coordinates, Field> map, int sizeOfMap) {
 		for (int i = 0; i < numberOfTrees; i++) {
-			Coordinates coordinates = generateCoordinatesForOther(map);
+			Coordinates coordinates = generateCoordinatesForOther(map, sizeOfMap);
 			map.get(coordinates).setTree(new Tree(coordinates));
 		}
 	}
 
-	private void placeStones(int numberOfStones, Map<Coordinates, Field> map) {
+	private void placeStones(int numberOfStones, Map<Coordinates, Field> map, int sizeOfMap) {
 		for (int i = 0; i < numberOfStones; i++) {
-			Coordinates coordinates = generateCoordinatesForOther(map);
+			Coordinates coordinates = generateCoordinatesForOther(map, sizeOfMap);
 			map.get(coordinates).setStone(new Stone(coordinates));
 		}
 	}
 
-	private void placePowerUp(int numberOfPowerUps, Map<Coordinates, Field> map) {
+	private void placePowerUp(int numberOfPowerUps, Map<Coordinates, Field> map, int sizeOfMap) {
 		for (int i = 0; i < numberOfPowerUps; i++) {
-			Coordinates coordinates = generateCoordinatesForOther(map);
+			Coordinates coordinates = generateCoordinatesForOther(map, sizeOfMap);
 			map.get(coordinates).setPowerUp(new LifePowerUp(coordinates));
 		}
 	}
 
+	/**
+	 * Generate number of Daleks based on levelNumber
+	 * 
+	 * @param levelNumber
+	 * @return
+	 */
 	private int generateNumberOfDaleks(int levelNumber) {
-		return 10;
+		return levelNumber * 5;
 	}
 
+	/**
+	 * Generate number of Trees based on levelNumber
+	 * 
+	 * @param levelNumber
+	 * @return
+	 */
 	private int generateNumberOfTrees(int levelNumber) {
-		return 2;
+		return levelNumber;
 	}
 
+	/**
+	 * Generate number of Stones based on levelNumber
+	 * 
+	 * @param levelNumber
+	 * @return
+	 */
 	private int generateNumberOfStones(int levelNumber) {
-		return 2;
+		return levelNumber;
 	}
 
+	/**
+	 * Generate number of PowerUps based on levelNumber
+	 * 
+	 * @param levelNumber
+	 * @return
+	 */
 	private int generateNumberOfPowerUps(int levelNumber) {
-		return 2;
+		return levelNumber;
 	}
 
-	private Coordinates generateCoordinatesForDoctor() {
-		return new Coordinates(1, 2);
+	/**
+	 * Generates coordinates for doctor
+	 * 
+	 * @param sizeOfMap
+	 * @return
+	 */
+	private Coordinates generateCoordinatesForDoctor(int sizeOfMap) {
+		return new Coordinates(sizeOfMap, sizeOfMap);
 	}
 
-	private Coordinates generateCoordinatesForOther(Map<Coordinates, Field> map) {
-		Coordinates coordinates = new Coordinates(1, 2);
+	/**
+	 * Generates coordinates for any object other than doctor. Coordinates are
+	 * pointed at empty field.
+	 * 
+	 * @param map
+	 * @param sizeOfMap
+	 * @return
+	 */
+	private Coordinates generateCoordinatesForOther(Map<Coordinates, Field> map, int sizeOfMap) {
+		Coordinates coordinates = new Coordinates(sizeOfMap, sizeOfMap);
 		while (thisFieldIsOccupiedByAnyObject(coordinates, map)) {
-			coordinates = new Coordinates(1, 2);
+			coordinates = new Coordinates(sizeOfMap, sizeOfMap);
 		}
 		return coordinates;
 	}
