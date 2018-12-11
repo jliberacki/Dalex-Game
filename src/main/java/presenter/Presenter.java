9956b1520595d1177095019ba762e212f17d8893
Marcin Czerenko <marcin.czerenko@gmail.com>
2018-12-11 15:34:52 +0100

    Presenter - communication between Model and View

--- src/main/java/presenter/Presenter.java
+++ src/main/java/presenter/Presenter.java
@@ -1,5 +1,21 @@
 package presenter;
+import model.Game;
+import view.View;
 
 public class Presenter {
-
+	private View view;
+	private Game model;
+	
+	public Presenter(View view) {
+		this.model=new Game(this);
+		this.view=view;
+		this.model.startGame();
+	}
+	public void onPlayerMove(int x, int y) {
+		model.doctor.updateMove(x,y);
+		model.continueGame();
+	}
+	public void onRoundUpdate() {
+		view.setScene(model.currentLevelMap);
+	}
 }
