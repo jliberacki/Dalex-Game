188c51f3a3ada23e4335f4ad9b95402d49cacb1b
jjano <janliberacki@gmail.com>
2018-12-19 10:35:36 +0100

    View refactor

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -25,6 +25,10 @@ public class View extends Application {
 	private GridPane root;
 	public int currentX;
 	public int currentY;
+	public int size;
+	public int windowSize;
+	
+	static final String squareStyle = "-fx-background-color: white; -fx-border-color: black;";
 	
 	//public Image img = new Image("http://android-boom.pl/uploads/posts/2016-04/1460641874_candycons-icon-pack_1.png");
 	public Image doctor = new Image("images/doctor.png");
@@ -35,19 +39,32 @@ public class View extends Application {
     	
         GridPane root = new GridPane();
         this.root = root;
-        this.currentX = (int)(Math.random() * 8);
-        this.currentY = (int)(Math.random() * 8);
+        this.size = 8;
+        this.windowSize = 400;
+        this.currentX = (int)(Math.random() * size);
+        this.currentY = (int)(Math.random() * size);
         
-        final int size = 8 ;
-        for (int row = 0; row < size; row++) {
+        drawBoard();
+        
+        Scene scene = createScene(size-1);
+        
+        //drawObject(0,0,dalek);
+        //moveObject(0,0,0,1,dalek);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void drawBoard () {
+    	for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col ++) {
                 StackPane square = new StackPane();
-                square.setStyle("-fx-background-color: white; -fx-border-color: black;");
+                square.setStyle(squareStyle);
                 ImageView imgView = new ImageView();
                 if(row == this.currentX && col == this.currentY) imgView.setImage(this.doctor);
                 else imgView.setImage(null);
-                imgView.setFitHeight(45);
-                imgView.setFitWidth(45);
+                imgView.setFitHeight(windowSize / size - 5);
+                imgView.setFitWidth(windowSize / size - 5);
                 square.getChildren().add(imgView);
                 this.root.add(square, col, row);
             }
@@ -56,32 +73,31 @@ public class View extends Application {
             this.root.getColumnConstraints().add(new ColumnConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, HPos.CENTER, true));
             this.root.getRowConstraints().add(new RowConstraints(5, Control.USE_COMPUTED_SIZE, Double.POSITIVE_INFINITY, Priority.ALWAYS, VPos.CENTER, true));
         }
-        
-        Scene scene = new Scene(this.root, 400, 400);
+    }
+    
+    public Scene createScene(int boardWidth) {
+    	
+    	Scene scene = new Scene(this.root, windowSize, windowSize);
         
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 switch (event.getCode()) {
                     case UP:    if(currentX>0) moveObject(currentX, currentY, currentX-1,currentY, doctor); break;
-                    case DOWN:  if(currentX<7) moveObject(currentX, currentY, currentX+1,currentY, doctor); break;
+                    case DOWN:  if(currentX<boardWidth) moveObject(currentX, currentY, currentX+1,currentY, doctor); break;
                     case LEFT:  if(currentY>0) moveObject(currentX, currentY, currentX,currentY-1, doctor); break;
-                    case RIGHT: if(currentY<7) moveObject(currentX, currentY, currentX,currentY+1, doctor); break;
+                    case RIGHT: if(currentY<boardWidth) moveObject(currentX, currentY, currentX,currentY+1, doctor); break;
                     default: break;
                 }
             }
         });
         
-        drawObject(0,0,dalek);
-        moveObject(0,0,0,1,dalek);
-        
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        return scene;
     }
     
     public void moveObject(int currentX, int currentY, int newX, int newY, Image img) {
-    	StackPane oldSquare = (StackPane) (this.root.getChildren().get(currentX*8+currentY));
-    	StackPane newSquare = (StackPane) (this.root.getChildren().get(newX*8+newY));
+    	StackPane oldSquare = (StackPane) (this.root.getChildren().get(currentX*size+currentY));
+    	StackPane newSquare = (StackPane) (this.root.getChildren().get(newX*size+newY));
     	ImageView oldImgView = (ImageView) oldSquare.getChildren().get(0);
     	ImageView newImgView = (ImageView) newSquare.getChildren().get(0);
     	
@@ -93,14 +109,14 @@ public class View extends Application {
     }
     
     public void drawObject(int X, int Y, Image img) {
-    	StackPane Square = (StackPane) (this.root.getChildren().get(X*8+Y));
+    	StackPane Square = (StackPane) (this.root.getChildren().get(X*size+Y));
     	ImageView ImgView = (ImageView) Square.getChildren().get(0);
     	
         ImgView.setImage(img);
     }
     
     public void removeObject(int X, int Y, Image img) {
-    	StackPane Square = (StackPane) (this.root.getChildren().get(X*8+Y));
+    	StackPane Square = (StackPane) (this.root.getChildren().get(X*size+Y));
     	ImageView ImgView = (ImageView) Square.getChildren().get(0);
     	
         ImgView.setImage(img);
